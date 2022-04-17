378e1d2f41c9b33a6480f6e49b77e5825076dfed
Nyasha Muusha <nyashamuusha@gmail.com>
2022-04-17 15:04:58 +0200

    first commit

---
+++ CamelCaseMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CamelCaseMethod
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      CamelCase("hello case");
+    }
+    public static string CamelCase(string str)
+    {
+      List<string> separatedWords = str.Split(' ').ToList();
+      List<string> result = new List<string>();
+      
+      while (separatedWords.Contains("") || separatedWords.Contains(" "))
+      {
+        separatedWords.Remove(""); // Remove Any Empty String
+        separatedWords.Remove(" "); // Remove Any Whitespace Characters
+      }
+      foreach (var separatedWord in separatedWords)
+      {
+        string convertedWord = char.ToUpper(separatedWord[0]) + separatedWord.Substring(1);
+        result.Add(convertedWord);
+      }
+      return string.Join("", result);
+     
+    }
+  }
+}
